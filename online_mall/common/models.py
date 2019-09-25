903cfbbf1fb01af37ee54f8568effb43eaed7fff
aisong_data_processing <yg247350078@163.com>
2019-09-25 10:59:28 +0800

    TEST

--- online_mall/common/models.py
+++ online_mall/common/models.py
@@ -132,6 +132,7 @@ class SecondColorSelector(models.Model):
 
     @classmethod
     def get_point_color(cls, color_code):
+        print(color_code)
         color_obj = cls.objects.get(color_code=color_code)
 
         return color_obj.name
