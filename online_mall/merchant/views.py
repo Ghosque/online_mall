81826fafba5cf1fd49665287c3b11f0285e012c7
Ghosque <40757278+ghosque@users.noreply.github.com>
2019-10-12 15:23:28 +0800

    更新封面数据保存oss_object

--- online_mall/merchant/views.py
+++ online_mall/merchant/views.py
@@ -482,7 +482,7 @@ class CommodityViewset(viewsets.ViewSet):
         while Commodity.objects.filter(commodity_id=commodity_id):
             commodity_id = GetId.getDigitId()
         # 封面
-        cover = self.save_base64_image(request.data['cover'], user_id, 'cover')
+        cover_obj = self.save_base64_image(request.data['cover'], user_id, 'cover')
         # 分类
         category = ThirdCategory.objects.get(id=request.data['category'])
         # 自定义属性
@@ -496,7 +496,7 @@ class CommodityViewset(viewsets.ViewSet):
                     name=request.data['name'],
                     title=request.data['title'],
                     title_desc=request.data['title_desc'],
-                    cover=cover,
+                    cover=cover_obj,
                     display_images=request.data['display_images'],
                     inventory=int(float(request.data['inventory'])),
                     price=float(request.data['price']),
@@ -619,13 +619,13 @@ class CommodityViewset(viewsets.ViewSet):
                 if 'cover' in data.keys():
                     cover = data['cover']
 
-                    original_cover = commodity.cover
+                    original_cover = settings.OSS_BUCKET.sign_url('GET', commodity.cover, 60)
                     base64_data = MerchantImage.get_image_base64_data(original_cover)
                     if base64_data == cover:
                         del data['cover']
                     else:
-                        cover_url = self.save_base64_image(base64_data, commodity.merchant.mall_user.user.id, 'cover')
-                        data['cover'] = cover_url
+                        cover_obj = self.save_base64_image(base64_data, commodity.merchant.mall_user.user.id, 'cover')
+                        data['cover'] = cover_obj
 
                 # 商品类别作为外键需要进一步处理
                 if 'category' in data.keys():
@@ -695,4 +695,4 @@ class CommodityViewset(viewsets.ViewSet):
             merchant=merchant
         )
 
-        return image_url
+        return MerchantImage.oss_object
