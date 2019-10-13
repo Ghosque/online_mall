661a4a1461db5e30b1a8fc69d4bdd1592179fb4f
Ghosque <40757278+ghosque@users.noreply.github.com>
2019-10-13 22:16:46 +0800

    更新oss图片处理方法

--- online_mall/merchant/views.py
+++ online_mall/merchant/views.py
@@ -482,7 +482,7 @@ class CommodityViewset(viewsets.ViewSet):
         while Commodity.objects.filter(commodity_id=commodity_id):
             commodity_id = GetId.getDigitId()
         # 封面
-        cover_obj = self.save_base64_image(request.data['cover'], user_id, 'cover')
+        cover = self.save_base64_image(request.data['cover'], user_id, 'cover')
         # 分类
         category = ThirdCategory.objects.get(id=request.data['category'])
         # 自定义属性
@@ -496,7 +496,7 @@ class CommodityViewset(viewsets.ViewSet):
                     name=request.data['name'],
                     title=request.data['title'],
                     title_desc=request.data['title_desc'],
-                    cover=cover_obj,
+                    cover=cover,
                     display_images=request.data['display_images'],
                     inventory=int(float(request.data['inventory'])),
                     price=float(request.data['price']),
@@ -624,8 +624,8 @@ class CommodityViewset(viewsets.ViewSet):
                     if base64_data == cover:
                         del data['cover']
                     else:
-                        cover_obj = self.save_base64_image(base64_data, commodity.merchant.mall_user.user.id, 'cover')
-                        data['cover'] = cover_obj
+                        cover = self.save_base64_image(base64_data, commodity.merchant.mall_user.user.id, 'cover')
+                        data['cover'] = cover
 
                 # 商品类别作为外键需要进一步处理
                 if 'category' in data.keys():
@@ -695,4 +695,4 @@ class CommodityViewset(viewsets.ViewSet):
             merchant=merchant
         )
 
-        return img
+        return image_url
