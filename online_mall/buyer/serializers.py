396b914285048f1cfcf14608a693e6c4a66bf7df
Ghosque <40757278+ghosque@users.noreply.github.com>
2019-10-16 11:48:46 +0800

    买家注册登录

--- online_mall/buyer/serializers.py
+++ online_mall/buyer/serializers.py
@@ -1,9 +1,34 @@
+import re
+from django.core.cache import cache
+from django.conf import settings
 from rest_framework import serializers
+from django.contrib.auth import authenticate
 
 from .models import Buyer
+from common.models import MallUser
 
 
 class BuyerLoginSerializer(serializers.Serializer):
+    phone = serializers.CharField(required=True, write_only=True, max_length=11, label='手机号码',
+                                  error_messages={
+                                      "max_length": "手机号码格式错误",
+                                      "min_length": "手机号码格式错误"
+                                  },
+                                  help_text="手机号码")
+    password = serializers.CharField(required=True, write_only=True, max_length=500, label='密码', help_text="密码")
+
+    def validate_password(self, password):
+        MallUser_list = MallUser.objects.filter(phone=self.initial_data["phone"], is_merchant=0)
+        if MallUser_list:
+            mall_user = MallUser_list[0]
+            username = mall_user.user.username
+            if not authenticate(username=username, password=password):
+                raise serializers.ValidationError("密码错误")
+        else:
+            raise serializers.ValidationError("手机号码错误")
+
+
+class BuyerRegSerializer(serializers.Serializer):
     password = serializers.CharField(required=True, write_only=True, max_length=500, label='密码', help_text="密码")
     name = serializers.CharField(required=True, write_only=True, max_length=50, label="姓名",
                                  error_messages={
@@ -24,6 +49,11 @@ class BuyerLoginSerializer(serializers.Serializer):
                                         "min_length": "身份证号码格式错误"
                                     },
                                     help_text="身份证号码")
+    nickname = serializers.CharField(required=True, write_only=True, max_length=30, label="昵称",
+                                     error_messages={
+                                         "max_length": "昵称格式错误",
+                                     },
+                                     help_text="昵称")
     code_key = serializers.CharField(required=True, write_only=True, label='验证码key', help_text='验证码key')
     code = serializers.CharField(required=True, write_only=True, max_length=4, min_length=4, label="验证码",
                                  error_messages={
@@ -31,3 +61,39 @@ class BuyerLoginSerializer(serializers.Serializer):
                                      "min_length": "验证码格式错误"
                                  },
                                  help_text="验证码")
+
+    # 验证手机号码
+    def validate_phone(self, phone):
+
+        # 手机是否注册
+        if MallUser.objects.filter(phone=phone).count():
+            raise serializers.ValidationError("用户已经存在")
+
+        # 验证手机号码是否合法
+        if not re.match(settings.REGEX_PHONE, phone):
+            raise serializers.ValidationError("手机号码非法")
+
+        return phone
+
+    def validate_id_card(self, id_card):
+        # 检查最后一位校验码是否正确
+        front_17_list = [i for i in id_card[:-1]]
+        remainder = sum([int(i) * int(j) for i, j in zip(front_17_list, settings.COEFFICIENT_LIST)]) % 11
+        if not settings.REMAINDER_DICT.get(remainder) == id_card[-1]:
+            raise serializers.ValidationError("身份证错误")
+
+        merchant_info = MallUser.objects.filter(id_card=id_card)
+        if merchant_info:
+            raise serializers.ValidationError("该身份证已被注册")
+
+    def validate_code(self, code):
+        if not cache.get(self.initial_data['code_key']):
+            raise serializers.ValidationError("该验证码已过期，请重新获取")
+
+        true_code = cache.get(self.initial_data['code_key'])
+        if true_code.lower() != code.lower():
+            raise serializers.ValidationError("验证码错误")
+
+    class Meta:
+        model = Buyer
+        fields = ('password', 'name', 'gender', 'phone', 'id_card', 'code_key', 'code')
--- online_mall/buyer/views.py
+++ online_mall/buyer/views.py
@@ -1,7 +1,15 @@
-from rest_framework import viewsets
+from django.conf import settings
+from django.core.cache import cache
+from django.contrib.auth.models import User
+from rest_framework import viewsets, status
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.decorators import permission_classes
+from rest_framework_jwt.serializers import jwt_payload_handler, jwt_encode_handler
+
+from .serializers import BuyerLoginSerializer, BuyerRegSerializer
+from .models import Buyer
+from common.models import MallUser
 
 
 class BuyerViewset(viewsets.ViewSet):
@@ -20,11 +28,75 @@ class BuyerViewset(viewsets.ViewSet):
 
     @classmethod
     def handle_login(cls, request):
+        serializer = BuyerLoginSerializer(data=request.data)
+        if not serializer.is_valid():
+            result = {
+                'code': 0,
+                'data': None,
+                'message': str(serializer.errors.get('password')[0])
+            }
+            return Response(result, status=status.HTTP_200_OK)
+
+        mall_user = MallUser.objects.get(phone=request.data['phone'])
+        user = mall_user.user
+        payload = jwt_payload_handler(user)
+        token = jwt_encode_handler(payload)
+
+        cache.set(user.id, token, settings.REFRESH_SECONDS)
+
+        result = {
+            'code': 1,
+            'data': {
+                'token': token,
+                'user_id': user.id
+            },
+            'message': '登录成功'
+        }
+
+        return result
+
+    @classmethod
+    def get_buyer_id(cls):
         pass
 
     @classmethod
     def handle_register(cls, request):
-        pass
+        serializer = BuyerRegSerializer(data=request.data)
+        if not serializer.is_valid():
+            if serializer.errors.get('id_card'):
+                message = str(serializer.errors.get('id_card')[0])
+            else:
+                message = str(serializer.errors.get('code')[0])
+
+            result = {
+                'code': 0,
+                'data': None,
+                'message': message
+            }
+            return Response(result, status=status.HTTP_200_OK)
+
+        merchant_id = cls.get_buyer_id()
+
+        user = User.objects.create_user(username=merchant_id, password=request.data['password'], is_superuser=0,
+                                        is_staff=1)
+
+        mall_user = MallUser.objects.create(
+            name=request.data['name'],
+            gender=request.data['gender'],
+            phone=request.data['phone'],
+            id_card=request.data['id_card'],
+            is_merchant=1,
+            user=user
+        )
+        Buyer.objects.create()  # TODO
+
+        result = {
+            'code': 1,
+            'data': None,
+            'message': '注册成功'
+        }
+
+        return result
 
     @permission_classes([IsAuthenticated])
     def retrieve(self, request, pk):
