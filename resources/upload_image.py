2437c9760f07ca278c69b8fccf519ed35c8b4746
Ghosque <247350078@qq.com>
2020-01-07 23:06:01 +0800

    新增第一分类3的图片

--- resources/upload_image.py
+++ resources/upload_image.py
@@ -2,10 +2,10 @@
 import os
 import oss2
 import pymysql
-from oss2.headers import OSS_OBJECT_TAGGING
 
 update_sql = "UPDATE `{}` SET image='{}' WHERE id='{}'"
 table = 'merchant_thirdcategory'
+first = '3'
 
 db = pymysql.connect(host="47.107.183.166", user="root", password="mysql", database="online_mall", charset="utf8")
 cursor = db.cursor()
@@ -14,10 +14,10 @@ auth = oss2.Auth('LTAI4FwfrskcFinuqtNADg8Y', 'LrI2TbPEl467zC8qSgDpA87qIpDzsA')
 # Endpoint以杭州为例，其它Region请按实际情况填写。
 bucket = oss2.Bucket(auth, 'http://oss-cn-shenzhen.aliyuncs.com', 'ghosque-online-mall')
 
-base_path = r'D:\Yang\Ghosque\online_mall\online_mall\online_mall\media\category\2'
+base_path = './category/{}'.format(first)
 for image in os.listdir(base_path):
-    object_name = 'media/category/2/{}'.format(image)
-    file_path = os.path.join(base_path, image)
+    object_name = 'media/category/{}/{}'.format(first, image)
+    file_path = os.path.join(os.path.abspath(base_path), image)
 
     bucket.put_object_from_file(object_name, file_path)
     url = bucket.sign_url('GET', object_name, 5*12*30*24*60*60)
