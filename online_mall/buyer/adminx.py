a1692c6b26e2cb25967648e812a79579c0f8ce0b
Ghosque <247350078@qq.com>
2020-01-19 16:57:02 +0800

    配置admin后台

--- online_mall/buyer/adminx.py
+++ online_mall/buyer/adminx.py
@@ -2,7 +2,8 @@ import xadmin
 from import_export import resources, fields
 from import_export.widgets import ForeignKeyWidget
 
-from .models import Buyer, FollowCommodity, CommodityView, Province, City, Area, Address
+from .models import Buyer, FollowCommodity, FollowShop, CommodityView, ReduceCardTicket, DiscountCardTicket,\
+    Province, City, Area, Address
 from adminx import *
 
 
@@ -24,11 +25,38 @@ class FollowCommodityAdmin:
     readonly_fields = ('create_time', 'update_time',)
 
 
+@xadmin.sites.register(FollowShop)
+class FollowShopAdmin:
+    list_display = ('id', 'status', 'buyer', 'shop', 'create_time', 'update_time',)
+    list_filter = ('status', 'buyer', 'shop',)
+    search_fields = ('shop',)
+    ordering = ('id',)
+    readonly_fields = ('create_time', 'update_time',)
+
+
 @xadmin.sites.register(CommodityView)
 class CommodityViewAdmin:
     list_display = ('id', 'buyer', 'commodity', 'create_time', 'update_time',)
     list_filter = ('buyer', 'commodity',)
     search_fields = ('commodity',)
+    ordering = ('id',)
+    readonly_fields = ('create_time', 'update_time',)
+
+
+@xadmin.sites.register(ReduceCardTicket)
+class ReduceCardTicketAdmin:
+    list_display = ('id', 'quota', 'min_price_limit', 'deadline', 'commodity_limit', 'category_limit', 'buyer', 'create_time', 'update_time',)
+    list_filter = ('buyer', 'commodity_limit', 'category_limit',)
+    search_fields = ('buyer', 'commodity_limit', 'category_limit',)
+    ordering = ('id',)
+    readonly_fields = ('create_time', 'update_time',)
+
+
+@xadmin.sites.register(DiscountCardTicket)
+class DiscountCardTicketAdmin:
+    list_display = ('id', 'quota', 'min_price_limit', 'deadline', 'commodity_limit', 'category_limit', 'buyer', 'create_time', 'update_time',)
+    list_filter = ('buyer', 'commodity_limit', 'category_limit',)
+    search_fields = ('buyer', 'commodity_limit', 'category_limit',)
     ordering = ('id',)
     readonly_fields = ('create_time', 'update_time',)
 
